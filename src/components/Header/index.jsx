c246f3b7c88948ee2e6d6321eba5a627f592756b
Evhenii Lapshinov <koenigstag@gmail.com>
2021-07-31 15:05:36 +0300

    Add logo and buttons to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import ButtonLink from '../ETC/ButtonLink';
 import styles from './Header.module.sass';
+import Logo from './Logo';
 import NavList from './NavList';
 
 const Header = () => {
   return (
     <header className={styles.headerMain}>
+      <Logo />
       <NavList />
+      <div>
+        <ButtonLink
+          link='/account/login'
+          text='Войти'
+          title='Страница входа в ЛК'
+        />
+        <ButtonLink
+          link='/account/register'
+          text='Регистрация'
+          title='Страница регистрации на проекте'
+          type='blue'
+        />
+      </div>
     </header>
   );
 };
