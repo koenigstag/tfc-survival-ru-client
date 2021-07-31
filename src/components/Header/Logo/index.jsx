dcb642ca43e82994df69c09936f0e7d3513d547c
Evhenii Lapshinov <koenigstag@gmail.com>
2021-07-31 15:05:00 +0300

    Add logo component

---
+++ src/components/Header/Logo/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from "@/logo.png"
+import styles from "./Logo.module.sass"
+
+const Logo = () => {
+  return (
+    <Link to="/" className={styles.logoWrapper}>
+      <img src={logo} alt="TFC Survival Logo" />
+      <span>TFC-survival</span>
+    </Link>
+  );
+}
+
+export default Logo;
