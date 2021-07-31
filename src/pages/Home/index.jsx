84fa802c330dc77245c908adaf313ab8a7e76350
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-01 00:59:01 +0300

    Add home page content

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import logo from '@/logo.png';
-import styles from '@/App.module.sass'
+import ButtonLink from '@/components/ETC/ButtonLink';
+import styles from "./HomePage.module.sass";
 
 const HomePage = () => {
   return (
-    <div className={styles.reactIntro}>
-      <img src={logo} className={styles.AppLogo} alt='logo' />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className={styles.AppLink}
-        href='https://reactjs.org'
-        target='_blank'
-        rel='noopener noreferrer'
-      >
-        Learn React
-      </a>
+    <div className={styles.homePage}>
+      <h3>Добро пожаловать на TFC-survival, дружок</h3>
+      <div className={styles.homePageButtons}>
+        <ButtonLink
+          text='Наш дискорд сервер'
+          href='https://discord.gg/xaFCzGarws'
+          title='Discord сообщество проекта'
+        />
+        <ButtonLink
+          text='Скачать лаунчер'
+          link='/home/launcher'
+          title='Скачать лаунчер для игры на сервере'
+          type='blue'
+        />
+      </div>
+      <h5>Мы есть на мониторингах, голосуйте за нас</h5>
+      <div>
+        <ButtonLink
+          text='minecraftratig.ru'
+          href='https://minecraftrating.ru/vote/114193/'
+          title='Проголосовать'
+        />
+        <ButtonLink
+          text='monitoringminecraft.ru'
+          href='https://monitoringminecraft.ru/top/tfc-survival'
+          title='Проголосовать'
+        />
+      </div>
     </div>
   );
 };
