97fd2dfec6513c7a07e35f59a2838eb6866774fd
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-02 00:32:04 +0300

    Add profile buttons

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,54 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import cx from 'classnames';
+import ButtonLink from '../ETC/ButtonLink';
 import Logo from './Logo';
 import NavList from './NavList';
-import ButtonLink from '../ETC/ButtonLink';
 import BurgerMenu from './BurgerMenu';
-import cx from 'classnames';
+import { selectUser } from '@/app/slices/userSlice';
 import styles from './Header.module.sass';
 
 const Header = () => {
   const [statusShow, setShow] = useState(false);
-  const classNames = cx(styles.buttonGroup, {
+  const hideOnSmallScreen = cx(styles.buttonGroup, {
     [styles.showOnBurgerAction]: statusShow,
   });
+
+  const user = useSelector(selectUser);
 
   return (
     <header className={styles.headerMain}>
       <Logo className={styles.blockLogo} />
       <NavList status={statusShow} />
-      <div className={classNames} id='authButtons'>
-        <ButtonLink
-          link='/account/login'
-          text='Войти'
-          title='Страница входа в ЛК'
-        />
-        <ButtonLink
-          link='/account/register'
-          text='Регистрация'
-          title='Страница регистрации на проекте'
-          type='blue'
-        />
-      </div>
+      {user.nickname === null ? (
+        <div className={hideOnSmallScreen} id='authButtons'>
+          <ButtonLink
+            link='/account/login'
+            text='Войти'
+            title='Страница входа в ЛК'
+          />
+          <ButtonLink
+            link='/account/register'
+            text='Регистрация'
+            title='Страница регистрации на проекте'
+            type='blue'
+          />
+        </div>
+      ) : (
+        <div className={hideOnSmallScreen}>
+          <ButtonLink
+            link='/profile'
+            text='Личный кабинет'
+            title='Страница ЛК'
+          />
+          <ButtonLink
+            link='/'
+            text='Выйти'
+            title='Выйти из аккаунта'
+            type='blue'
+          />
+        </div>
+      )}
       <BurgerMenu action={setShow} status={statusShow} />
     </header>
   );
