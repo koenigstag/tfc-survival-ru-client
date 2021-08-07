a324a27a0e6e22469f7e81aa67f702f1d3b5a5f8
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-07 17:59:57 +0300

    Add register error rendering

--- src/pages/Auth/Register/RegisterForm/index.jsx
+++ src/pages/Auth/Register/RegisterForm/index.jsx
@@ -1,46 +1,50 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Field, Form, Formik } from 'formik';
 import FieldError from '@/components/ETC/FieldError';
-import { registerUserAsync } from '@/app/slices/userSlice';
+import { registerUserAsync, selectUser } from '@/app/slices/userSlice';
 import { registerScheme } from '@/validation/schemes';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
+  const user = useSelector(selectUser);
 
   return (
-    <Formik
-      initialValues={{
-        nickname: '',
-        email: '',
-        password: '',
-        confpassword: '',
-      }}
-      validationSchema={registerScheme}
-      onSubmit={(values, formikBag) => {
-        dispatch(registerUserAsync(values));
-        console.log(values);
-        formikBag.resetForm();
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form>
-          <Field type='text' name='nickname' />
-          <FieldError name='nickname' errors={errors} touched={touched} />
+    <>
+      {user.errorMessage ? <div>{user.errorMessage}</div> : <div></div>}
+      <Formik
+        initialValues={{
+          nickname: '',
+          email: '',
+          password: '',
+          confpassword: '',
+        }}
+        validationSchema={registerScheme}
+        onSubmit={(values, formikBag) => {
+          console.log(values);
+          dispatch(registerUserAsync(values));
+          formikBag.resetForm();
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field type='text' name='nickname' />
+            <FieldError name='nickname' errors={errors} touched={touched} />
 
-          <Field type='email' name='email' />
-          <FieldError name='email' errors={errors} touched={touched} />
+            <Field type='email' name='email' />
+            <FieldError name='email' errors={errors} touched={touched} />
 
-          <Field type='password' name='password' />
-          <FieldError name='password' errors={errors} touched={touched} />
+            <Field type='password' name='password' />
+            <FieldError name='password' errors={errors} touched={touched} />
 
-          <Field type='password' name='confpassword' />
-          <FieldError name='confpassword' errors={errors} touched={touched} />
+            <Field type='password' name='confpassword' />
+            <FieldError name='confpassword' errors={errors} touched={touched} />
 
-          <button type='submit'>Регистрация</button>
-        </Form>
-      )}
-    </Formik>
+            <button type='submit'>Регистрация</button>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
 
