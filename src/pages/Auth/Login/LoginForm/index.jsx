10a5695519b4657c00dbb68910ea2a1dba0f3291
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-07 23:40:55 +0300

    Add lofin form

---
+++ src/pages/Auth/Login/LoginForm/index.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Field, Form, Formik } from 'formik';
+import { loginUserAsync, selectUser } from '@/app/slices/userSlice';
+import { loginScheme } from '@/validation/schemes';
+import FieldError from '@/components/ETC/FieldError';
+
+const initialValues = {
+  nickname: '',
+  password: '',
+};
+
+const loginRUSErrors = {
+  'Invalid nickname or password': 'Неверный логин или пароль',
+};
+
+const LoginForm = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      {user.status === 'error' ? (
+        <div>{loginRUSErrors[user.errorMessage]}</div>
+      ) : (
+        <div></div>
+      )}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginScheme}
+        onSubmit={(values, formikBag) => {
+          console.log(values);
+          dispatch(loginUserAsync(values));
+          formikBag.resetForm();
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <Field type='text' name='nickname' />
+            <FieldError
+              name='nickname'
+              errors={errors}
+              touched={touched}
+              tag={'div'}
+            />
+
+            <Field type='password' name='password' />
+            <FieldError
+              name='password'
+              errors={errors}
+              touched={touched}
+              tag={'div'}
+            />
+
+            <button type='submit'>Логин</button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default LoginForm;
--- src/pages/Auth/Login/index.jsx
+++ src/pages/Auth/Login/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import LoginForm from './LoginForm';
 
 const LoginPage = () => {
   return (
     <div>
       <h4>Войти</h4>
+      <LoginForm />
     </div>
   );
-}
+};
 
 export default LoginPage;
