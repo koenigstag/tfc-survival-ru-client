2ac960268faada5b6e201a5722e0e3aa04ea03b5
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-08 22:18:45 +0300

    Remove lazy load

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React, { Suspense } from 'react';
+import React/*, { Suspense }*/ from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import App from './App.jsx';
-import Loader from './components/Loader';
+import App from './App.jsx';
 import { store } from '@/app/store';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
-const App = React.lazy(() => import('./App'));
+// const App = React.lazy(() => import('./App'));
+// import Loader from './components/Loader';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Suspense fallback={Loader}>
-        <App />
-      </Suspense>
+      {/* <Suspense fallback={Loader}> */}
+      <App />
+      {/* </Suspense> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
