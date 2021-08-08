cb80d2ab57ca0e0c1e1023621a1fcd00b546bb83
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-08 12:32:57 +0300

    Fix onChange error logging

--- src/components/Header/BurgerMenu/index.jsx
+++ src/components/Header/BurgerMenu/index.jsx
@@ -4,7 +4,12 @@ import styles from './BurgerMenu.module.sass';
 const BurgerMenu = ({ onClick, status }) => {
   return (
     <div className={styles.burgerMenu}>
-      <input type='checkbox' id='burgetMenuBox' checked={status} />
+      <input
+        type='checkbox'
+        id='burgetMenuBox'
+        checked={status}
+        onChange={() => {}}
+      />
       <label htmlFor='burgetMenuBox' onClick={onClick}>
         <div className={styles.burgerIcon}>
           <div></div>
