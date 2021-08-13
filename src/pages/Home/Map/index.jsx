2d8069bbaa34ef168756ec3b78e81c67acc4c7f6
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-13 16:27:49 +0300

    Add map background

--- src/pages/Home/Map/index.jsx
+++ src/pages/Home/Map/index.jsx
@@ -1,24 +1,43 @@
 import React from 'react';
 import { mapURL } from '@/api/';
+import blank_map from './media/images/blank_map.png';
 
 const MapPage = () => {
   return (
-    <iframe
+    <div
       style={{
+        background: `url(${blank_map}) no-repeat`,
+        backgroundSize: '100% 100%',
+        // backgroundPosition: 'center',
         position: 'absolute',
-        top: '0',
-        left: 0,
-        bottom: '0',
-        right: 0,
+        top: '1rem',
+        left: '1rem',
+        bottom: '1rem',
+        right: '1rem',
         zIndex: 1,
 
         margin: 'auto',
-        width: '95%',
-        height: 'calc(100% - 0.75rem)',
       }}
-      title='Tfc-survival map'
-      src={mapURL}
-    ></iframe>
+    >
+      <iframe
+        style={{
+          position: 'absolute',
+          top: '0',
+          left: 0,
+          bottom: '0',
+          right: 0,
+          zIndex: 2,
+
+          margin: 'auto',
+          width: '85%',
+          height: '80%',
+        }}
+        title='Tfc-survival map'
+        src={mapURL}
+      >
+        Something went wrong...
+      </iframe>
+    </div>
   );
 };
 
