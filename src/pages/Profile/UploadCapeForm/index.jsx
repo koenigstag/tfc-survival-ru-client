2ac90072893136d5f8e3caffcac501da24cc8c46
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-17 21:36:50 +0300

    Add upload skin and cape form

---
+++ src/pages/Profile/UploadCapeForm/index.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Field, Form, Formik } from 'formik';
+import { setCapeAsync } from '@/app/slices/userSlice';
+
+const initialValues = {
+  cape: null,
+};
+
+const UploadCapeForm = ({ user }) => {
+  const dispatch = useDispatch();
+
+  // load skin
+  useEffect(() => {
+    // effect
+    return () => {
+      // cleanup
+    };
+  }, []);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, formikBag) => {
+        dispatch(
+          setCapeAsync({
+            nickname: user.data.nickname,
+            cape: values.cape,
+          })
+        );
+
+        formikBag.resetForm();
+      }}
+    >
+      {() => (
+        <Form>
+          <fieldset>
+            <legend>Загрузка плаща</legend>
+            <div>Файл плаща в формате png</div>
+            <Field type='file' name='cape' />
+          </fieldset>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default UploadCapeForm;
---
+++ src/pages/Profile/UploadSkinForm/index.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Field, Form, Formik } from 'formik';
+import { setSkinAsync } from '@/app/slices/userSlice';
+
+const initialValues = {
+  skin: null,
+};
+
+const UploadSkinForm = ({ user }) => {
+  const dispatch = useDispatch();
+
+  // load cape
+  useEffect(() => {
+    // effect
+    return () => {
+      // cleanup
+    };
+  }, []);
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, formikBag) => {
+        dispatch(
+          setSkinAsync({
+            nickname: user.data.nickname,
+            cape: values.cape,
+          })
+        );
+
+        formikBag.resetForm();
+      }}
+    >
+      {() => (
+        <Form>
+          <fieldset>
+            <legend>Загрузка скина</legend>
+            <div>Файл скина в формате png</div>
+            <Field type='file' name='skin' />
+          </fieldset>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default UploadSkinForm;
