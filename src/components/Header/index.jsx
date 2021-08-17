7eef4f26210f2607553fee315838ec15cd8f61f9
Evhenii Lapshinov <koenigstag@gmail.com>
2021-08-17 17:17:46 +0300

    Add back button for burger menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -28,44 +28,64 @@ const Header = () => {
   const user = useSelector(selectUserData);
 
   return (
-    <header className={styles.headerMain}>
-      <Logo className={styles.blockLogo} />
-      <NavList status={statusShow} onClick={hideOrShow} />
-      {user.nickname === null ? (
-        <div className={hideOnSmallScreen} id='authButtons'>
-          <ButtonLink
-            link='/account/login'
-            text='Войти'
-            title='Страница входа в ЛК'
-            onClick={hideOrShow}
-          />
-          <ButtonLink
-            link='/account/register'
-            text='Регистрация'
-            title='Страница регистрации на проекте'
-            type='blue'
-            onClick={hideOrShow}
-          />
-        </div>
-      ) : (
-        <div className={hideOnSmallScreen}>
-          <ButtonLink
-            link='/profile'
-            text='Личный кабинет'
-            title='Страница ЛК'
-            onClick={hideOrShow}
-          />
-          <ButtonLink
-            link='/'
-            text='Выйти'
-            title='Выйти из аккаунта'
-            type='blue'
-            onClick={logout}
-          />
-        </div>
+    <>
+      <header
+        className={styles.headerMain}
+        style={{ position: 'relative', zIndex: 2 }}
+      >
+        <Logo className={styles.blockLogo} />
+        <NavList status={statusShow} onClick={hideOrShow} />
+        {user.nickname === null ? (
+          <div className={hideOnSmallScreen} id='authButtons'>
+            <ButtonLink
+              link='/account/login'
+              text='Войти'
+              title='Страница входа в ЛК'
+              onClick={hideOrShow}
+            />
+            <ButtonLink
+              link='/account/register'
+              text='Регистрация'
+              title='Страница регистрации на проекте'
+              type='blue'
+              onClick={hideOrShow}
+            />
+          </div>
+        ) : (
+          <div className={hideOnSmallScreen}>
+            <ButtonLink
+              link='/profile'
+              text='Личный кабинет'
+              title='Страница ЛК'
+              onClick={hideOrShow}
+            />
+            <ButtonLink
+              link='/'
+              text='Выйти'
+              title='Выйти из аккаунта'
+              type='blue'
+              onClick={logout}
+            />
+          </div>
+        )}
+        <BurgerMenu onClick={hideOrShow} status={statusShow} />
+      </header>
+      {statusShow && (
+        <div
+          style={{
+            position: 'fixed',
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            backgroundColor: "#aaa",
+            opacity: "0.75",
+            zIndex: 1,
+          }}
+          onClick={hideOrShow}
+        ></div>
       )}
-      <BurgerMenu onClick={hideOrShow} status={statusShow} />
-    </header>
+    </>
   );
 };
 
