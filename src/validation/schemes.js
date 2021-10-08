f3b346e8b01a19396855a4e97e38b2db176c7c6c
Evhenii Lapshinov <koenigstag@gmail.com>
2021-10-09 01:13:17 +0300

    Add regex vars

--- src/validation/schemes.js
+++ src/validation/schemes.js
@@ -1,37 +1,49 @@
 import * as Yup from 'yup';
 
-// TODO regexp
+// regexp
+const usernameRegex = /^[a-z0-9_]{3,16}$/i;
+const emailRegex = /^\S+@\S+\.\S+$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])[0-9a-z]{6,}$/i;
+const discordRegex = /^.{2,32}#\d{4}$/;
+const tokenRegex = /^\$2[a-z0-9.\/$]{58}$/i;
+const skinFilenameRegex = /^[a-z0-9_\-\/]{3,16}.png$/i;
+
+// schemes
 const nicknameScheme = Yup.string()
   .matches(
-    /^[a-z0-9_]{3,16}$/i,
+    usernameRegex,
     'Разрешенная длина ника от 3 до 16 символов. Используйте только латинские буквы, числа и символ подчеркивания'
   )
   .required('Требуемое поле');
-const accessTokenScheme = Yup.string()
-  .length(32)
-  .matches();
-const discordScheme = Yup.string().matches(
-  /^.{2,32}#[0-9]{4}$/,
-  'Не соответствует шаблону Discord tag'
-);
+
 const emailScheme = Yup.string()
   .email('Не соответствует формату email')
-  .matches(/^\S+@\S+\.\S+$/, 'Не соответствует шаблону')
+  .matches(emailRegex, 'Не соответствует шаблону')
   // TODO test whitelisted email domains
   .test(() => {
     return true;
   })
   .required('Требуемое поле');
+
+const discordScheme = Yup.string().matches(
+  discordRegex,
+  'Не соответствует шаблону Discord tag'
+);
+
+const accessTokenScheme = Yup.string().matches(tokenRegex);
+
 const passwordScheme = Yup.string()
   .matches(
-    /^(?=.*\d)(?=.*[a-z])[0-9a-z]{6,}$/i,
+    passwordRegex,
     'Не соответствует шаблону. Минимум 6 символов: цифр, и латинских букв'
   )
   .required('Требуемое поле');
+
 const confpasswordScheme = Yup.string()
   .oneOf([Yup.ref('password'), null], 'Пароли должны совпадать')
   .required('Повторите пароль');
 
+// forms schemes
 export const registerScheme = Yup.object().shape({
   nickname: nicknameScheme,
   email: emailScheme,
