a4fa198fac943b1a09994611458cf26eee7ef479
Evhenii Lapshinov <koenigstag@gmail.com>
2021-10-10 16:13:19 +0300

    Add skin and cape api calls

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,5 +1,6 @@
-import { client } from './index';
-import { encrypt } from '@/services/passTransfer';
+import axios from 'axios';
+import { baseURL, clientApi } from './index';
+import { encrypt } from 'services/passTransfer';
 
 // TODO check not needed
 /* export const getUser = async (nickname, accessToken, refreshToken) => {
@@ -13,7 +14,7 @@ import { encrypt } from '@/services/passTransfer';
 }; */
 
 export const registerUser = async ({ user, password, ua }) => {
-  const response = await client.post('users', {
+  const response = await clientApi.post('users', {
     // TODO more useragent data
     user,
     ua,
@@ -28,7 +29,7 @@ export const registerUser = async ({ user, password, ua }) => {
 };
 
 export const loginUser = async ({ nickname, password }) => {
-  const user = await client.post(`users/${nickname}`, {
+  const user = await clientApi.post(`users/${nickname}`, {
     passwordCrypt: encrypt(password),
   });
 
@@ -40,7 +41,7 @@ export const loginUser = async ({ nickname, password }) => {
 };
 
 export const changePass = async ({ nickname, password, oldpassword }) => {
-  const user = await client.patch(`users/${nickname}`, {
+  const user = await clientApi.patch(`users/${nickname}`, {
     passwordCrypt: encrypt(password),
     oldpassword,
   });
@@ -53,7 +54,7 @@ export const changePass = async ({ nickname, password, oldpassword }) => {
 };
 
 export const linkDiscord = async ({ nickname, discord }) => {
-  const user = await client.patch(`users/discord/${nickname}`, {
+  const user = await clientApi.patch(`users/discord/${nickname}`, {
     discord,
   });
 
@@ -62,4 +63,52 @@ export const linkDiscord = async ({ nickname, discord }) => {
   }
 
   return user;
+};
+
+// TODO test api
+export const setSkin = async ({ nickname, accessToken, file }) => {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('accessToken', accessToken);
+
+  const res = await clientApi.post(`media/skin/${nickname}`, data);
+
+  if (res.status !== 200) {
+    throw new Error('Cannot set skin');
+  }
+
+  return baseURL + `/static/skins/${nickname}.png`;
+};
+
+export const setCape = async ({ nickname, accessToken, file }) => {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('accessToken', accessToken);
+  const res = await clientApi.post(`media/cape/${nickname}`, data);
+
+  if (res.status !== 200) {
+    throw new Error('Cannot set skin');
+  }
+
+  return baseURL + `/static/capes/${nickname}.png`;
+};
+
+export const getSkin = async ({ nickname }) => {
+  const res = await axios.get(`${baseURL}/static/skins/${nickname}.png`);
+
+  if (res.status !== 200) {
+    throw new Error('Cannot get skin');
+  }
+
+  return baseURL + `/static/skins/${nickname}.png`;
+};
+
+export const getCape = async ({ nickname }) => {
+  const res = await axios.get(`${baseURL}/static/capes/${nickname}.png`);
+
+  if (res.status !== 200) {
+    throw new Error('Cannot get cape');
+  }
+
+  return baseURL + `/static/capes/${nickname}.png`;
 };
