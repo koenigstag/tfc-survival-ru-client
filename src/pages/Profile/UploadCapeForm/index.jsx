351b396365fb9e660a5ecfd3e4e8f4575fb889bd
Evhenii Lapshinov <koenigstag@gmail.com>
2021-10-10 16:15:34 +0300

    Add proper skin and cape api calls

--- src/pages/Profile/UploadCapeForm/index.jsx
+++ src/pages/Profile/UploadCapeForm/index.jsx
@@ -1,46 +1,101 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Field, Form, Formik } from 'formik';
-import { setCapeAsync } from '@/app/slices/userSlice';
+import React, { useEffect, useState } from 'react';
+import { Form, Formik } from 'formik';
+import { getCape, setCape } from '../../../api/userAPI';
 
 const initialValues = {
   cape: '',
 };
 
 const UploadCapeForm = ({ user }) => {
-  const dispatch = useDispatch();
+  const [capeSrc, setCapeSrc] = useState(null);
 
-  // load skin
   useEffect(() => {
-    // effect
-    return () => {
-      // cleanup
-    };
-  }, []);
+    getCape({ nickname: user.data.nickname })
+      .then(res => {
+        setCapeSrc(res);
+      })
+      .catch(e => {
+        // console.error(e);
+      });
+  }, [user.data.nickname]);
 
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={(values, formikBag) => {
-        dispatch(
-          setCapeAsync({
-            nickname: user.data.nickname,
-            cape: values.cape,
-          })
-        );
+        if (!values.cape) {
+          return;
+        }
+        console.log('submit cape');
+
+        setCape({
+          nickname: user.data.nickname,
+          accessToken: user.data.accessToken,
+          file: values.cape,
+        }).then(res => {
+          setCapeSrc(res);
+        });
 
         formikBag.resetForm();
       }}
     >
-      {() => (
-        <Form>
-          <fieldset>
-            <legend>Загрузка плаща</legend>
-            <div>Файл плаща в формате png</div>
-            <Field type='file' name='cape' />
-          </fieldset>
-        </Form>
-      )}
+      {({ values, errors, setFieldValue, setFieldError }) => {
+        const onChangeFile = ({
+          target: {
+            files: [file],
+            name,
+          },
+        }) => {
+          if (!file) {
+            return;
+          }
+
+          if (file.type !== 'image/png') {
+            setFieldError(name, 'Неверный тип файла');
+            return;
+          }
+          if (file.size >= 800000) {
+            setFieldError(name, 'Размер файла не может быть больше 800кб');
+            return;
+          }
+
+          setFieldValue(name, file);
+        };
+
+        const style = {
+          marginTop: '10px',
+          maxWidth: '300px',
+          // border: '1px solid #555',
+          // borderRadius: '5px',
+          outline: 'thick double #555',
+        };
+
+        return (
+          <>
+            <Form>
+              <fieldset>
+                <legend>Загрузка плаща</legend>
+                <div>Файл плаща в формате png</div>
+                <input type='file' name='cape' onChange={onChangeFile} />
+
+                <div>
+                  <button type='submit' disabled={errors.cape || !values.cape}>
+                    Установить
+                  </button>{' '}
+                  {errors.cape ? errors.cape : ''}
+                </div>
+                <div>
+                  {capeSrc === null ? (
+                    'Плащ не установлен'
+                  ) : (
+                    <img style={style} src={capeSrc} alt='user cape' />
+                  )}
+                </div>
+              </fieldset>
+            </Form>
+          </>
+        );
+      }}
     </Formik>
   );
 };
--- src/pages/Profile/UploadSkinForm/index.jsx
+++ src/pages/Profile/UploadSkinForm/index.jsx
@@ -1,46 +1,110 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { Field, Form, Formik } from 'formik';
-import { setSkinAsync } from '@/app/slices/userSlice';
+import React, { useEffect, useState } from 'react';
+import { Form, Formik } from 'formik';
+import { getSkin, setSkin } from '../../../api/userAPI';
+import { baseURL } from '../../../api';
 
 const initialValues = {
   skin: '',
 };
 
 const UploadSkinForm = ({ user }) => {
-  const dispatch = useDispatch();
+  const [skinSrc, setSkinSrc] = useState(null);
 
-  // load cape
   useEffect(() => {
-    // effect
-    return () => {
-      // cleanup
-    };
-  }, []);
+    getSkin({ nickname: user.data.nickname })
+      .then(res => {
+        setSkinSrc(res);
+      })
+      .catch(e => {
+        // console.error(e);
+      });
+  }, [user.data.nickname]);
 
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={(values, formikBag) => {
-        dispatch(
-          setSkinAsync({
-            nickname: user.data.nickname,
-            cape: values.cape,
+        if (!values.skin) {
+          return;
+        }
+        console.log('submit skin');
+
+        setSkin({
+          nickname: user.data.nickname,
+          accessToken: user.data.accessToken,
+          file: values.skin,
+        })
+          .then(res => {
+            setSkinSrc(res);
           })
-        );
+          .catch(e => {
+            // console.error(e);
+          });
 
         formikBag.resetForm();
       }}
     >
-      {() => (
-        <Form>
-          <fieldset>
-            <legend>Загрузка скина</legend>
-            <div>Файл скина в формате png</div>
-            <Field type='file' name='skin' />
-          </fieldset>
-        </Form>
-      )}
+      {({ values, errors, setFieldValue, setFieldError }) => {
+        const onChangeFile = ({
+          target: {
+            files: [file],
+            name,
+          },
+        }) => {
+          if (!file) {
+            return;
+          }
+
+          if (file.type !== 'image/png') {
+            setFieldError(name, 'Неверный тип файла');
+            return;
+          }
+          if (file.size >= 800000) {
+            setFieldError(name, 'Размер файла не может быть больше 800кб');
+            return;
+          }
+
+          setFieldValue(name, file);
+        };
+
+        const style = {
+          marginTop: '10px',
+          maxWidth: '300px',
+          // border: '1px solid #555',
+          // borderRadius: '5px',
+          outline: 'thick double #555',
+        };
+
+        return (
+          <>
+            <Form>
+              <fieldset>
+                <legend>Загрузка скина</legend>
+                <div>Файл скина в формате png</div>
+                <input type='file' name='skin' onChange={onChangeFile} />
+                <div>
+                  <button type='submit' disabled={errors.skin || !values.skin}>
+                    Установить
+                  </button>{' '}
+                  {errors.skin ? errors.skin : ''}
+                </div>
+                <div>
+                  {skinSrc === null ? (
+                    <img
+                      style={style}
+                      src={`${baseURL}/static/skins/steve.png`}
+                      alt='default skin'
+                    />
+                  ) : (
+                    <img style={style} src={skinSrc} alt='user skin' />
+                  )}
+                </div>
+              </fieldset>
+            </Form>
+            <iframe title="render 3d" src="https://minerender.org/embed/skin/?skin=MiniDigger&shadow=true" frameborder="0"></iframe>
+          </>
+        );
+      }}
     </Formik>
   );
 };
