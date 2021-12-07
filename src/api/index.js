e60ec6ca2930294fd644b8b685fd6093ffdcba14
Evhenii Lapshinov <freshjs.fte@gmail.com>
2021-12-07 15:49:55 +0200

    Refactor interceptors

--- src/api/index.js
+++ src/api/index.js
@@ -1,43 +1,70 @@
-import axios from 'axios';
-import CONTANTS from '../constants';
-import history from '../browserHistory';
+import axios from "axios";
+import CONSTANTS from "../constants";
+import history from "../browserHistory";
+import { removeTokens, refreshUser, saveTokens } from "./authAPI";
 
-export const baseURL = 'http://localhost:5001';
+export const baseURL = "http://localhost:5001";
 // const baseURL = 'http://tfc-survival.ru:5001';
 
 const clientApi = axios.create({
   baseURL: `${baseURL}/api/`,
 });
 
-export const mapURL = 'http://tfc-survival.ru:8154/';
+export const mapURL = "http://tfc-survival.ru:8154/";
 
 /* interceptors */
 clientApi.interceptors.request.use(
-  config => {
-    const token = window.localStorage.getItem(CONTANTS.ACCESS_TOKEN);
+  (config) => {
+    const token = window.localStorage.getItem(CONSTANTS.ACCESS_TOKEN);
     if (token) {
       config.headers = { ...config.headers, Authorization: "Bearer " + token };
     }
     return config;
   },
-  err => Promise.reject(err)
+  (err) => Promise.reject(err)
 );
 
 clientApi.interceptors.response.use(
-  response => {
-    if (response.data.token) {
-      window.localStorage.setItem(CONTANTS.ACCESS_TOKEN, response.data.token);
+  (response) => {
+    if (response.data.data.tokenPair) {
+      saveTokens(response.data.data.tokenPair);
     }
     return response;
   },
-  err => {
+  async (err) => {
     if (
-      err.response.status === 408 &&
-      history.location.pathname !== '/account/login' &&
-      history.location.pathname !== '/account/register' &&
-      history.location.pathname !== '/'
+      err.response.status === 401 &&
+      history.location.pathname !== "/account/login" &&
+      history.location.pathname !== "/account/register" &&
+      history.location.pathname !== "/"
     ) {
-      history.replace('/account/login');
+      const refreshToken = localStorage.getItem(CONSTANTS.REFRESH_TOKEN);
+      if (refreshToken) {
+        try {
+          const { tokenPair } = await refreshUser(refreshToken);
+          saveTokens(tokenPair);
+
+          err.config.headers = {
+            ...err.config.headers,
+            Authorization: "Bearer " + tokenPair.access,
+          };
+          clientApi.request(err.config);
+          return;
+        } catch (error) {
+          removeTokens();
+          history.replace("/account/login");
+        }
+      }
+    }
+
+    if (
+      err.response.status === 419 &&
+      history.location.pathname !== "/account/login" &&
+      history.location.pathname !== "/account/register" &&
+      history.location.pathname !== "/"
+    ) {
+      removeTokens();
+      history.replace("/account/login");
     }
     return Promise.reject(err);
   }
