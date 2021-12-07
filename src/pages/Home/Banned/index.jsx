d7cf48598c6604a5bfd4263ae8f1491c61f14742
Evhenii Lapshinov <freshjs.fte@gmail.com>
2021-12-07 18:26:26 +0200

    Add banned list

--- src/pages/Home/Banned/index.jsx
+++ src/pages/Home/Banned/index.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import TableRow from './TableRow';
+import Table from 'components/ETC/Table';
+import bannedList from 'banned-players.json'
 import styles from './BannedPage.module.sass';
 
 const BannedPage = () => {
-  // api call
-  const bannedArray = [];
-
   return (
     <>
       <h4>Забаненные игроки</h4>
       <div className={styles.tableWrapper}>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Ники</th>
-              <th>Дата</th>
-              <th>Кем забанен</th>
-              <th>Когда истекает</th>
-              <th>Причина</th>
-            </tr>
-          </thead>
-          <tbody>
-            {bannedArray.map(data => (
-              <TableRow data={data} />
-            ))}
-          </tbody>
-        </table>
+        <Table
+          className={styles.table}
+          list={bannedList}
+          sortFunc={(item1, item2) => {
+            if (item2.created > item1.created) {
+              return 1
+            }
+            if (item1.created > item2.created) {
+              return -1
+            }
+            return 0
+          }}
+          headers={['Ник', 'Дата', "Кем забанен", "Когда истекает", "Причина"]}
+          paths={['name', 'created', 'source', 'expires', 'reason']}
+          itemKey="uuid"
+        />
       </div>
     </>
   );
