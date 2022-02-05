ed4af82ed84630d2360288ebb83059af2283c98b
Yevhenii <koenigstag@gmail.com>
2022-02-06 00:46:46 +0200

    Add discord widget button to footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Copyright from './Copyright';
 import styles from './Footer.module.sass';
 
 const Footer = () => {
+  const [discordBlock, setDiscordBlock] = useState(false);
+  const handleDiscordShow = useCallback(() => {
+    setDiscordBlock((s) => !s);
+  }, []);
+
   return (
     <footer className={styles.footer}>
       <Copyright />
+      <div style={{ position: "absolute", bottom: "0px", right: "25px", zIndex: 2 }}>
+        <img onClick={handleDiscordShow} className={styles.homeDiscordButton} src="/dis-comb.png" alt="discord logo" />
+      </div>
+      <div
+        style={{
+          display: discordBlock ? "block" : "none",
+          position: "fixed",
+          right: "5px",
+          bottom: "5px",
+          zIndex: 3,
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "#202225",
+            color: "white",
+            margin: "0 auto",
+            borderRadius: "10px 10px 0 0",
+            position: "relative",
+            bottom: "-5px",
+            zIndex: 2,
+            paddingRight: "10px",
+            textAlign: "right",
+            cursor: "pointer",
+          }}
+          onClick={handleDiscordShow}
+        >
+          X
+        </div>
+        <iframe
+          title="discord-widget"
+          src="https://discord.com/widget?id=253218452209270784&amp;theme=dark"
+          width="280"
+          height="400"
+          allowtransparency="true"
+          frameborder="0"
+          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+        ></iframe>
+      </div>
     </footer>
   );
 };
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
-import ButtonLink from "components/ETC/ButtonLink";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUser } from "app/slices/userSlice";
 import { getVKNews } from "api/commonAPI";
@@ -7,10 +6,6 @@ import styles from "./HomePage.module.sass";
 
 const HomePage = () => {
   const user = useSelector(selectUser);
-  const [discordBlock, setDiscordBlock] = useState(false);
-  const handleDiscordShow = useCallback(() => {
-    setDiscordBlock((s) => !s);
-  }, []);
 
   const [news, setNews] = useState([]);
 
@@ -28,42 +23,6 @@ const HomePage = () => {
         Добро пожаловать на TFC-survival,{" "}
         {user.isAuth ? user.data.nickname : "дружок"}
       </h3>
-      <div
-        style={{
-          display: discordBlock ? "block" : "none",
-          position: "fixed",
-          right: "5px",
-          bottom: "5px",
-          zIndex: 3,
-        }}
-      >
-        <div
-          style={{
-            backgroundColor: "#202225",
-            color: "white",
-            margin: "0 auto",
-            borderRadius: '10px 10px 0 0',
-            position: 'relative',
-            bottom: '-5px',
-            zIndex: 2,
-            paddingRight: '10px',
-            textAlign: 'right'
-          }}
-          onClick={handleDiscordShow}
-        >
-          X
-        </div>
-        <iframe
-          title="discord-widget"
-          src="https://discord.com/widget?id=253218452209270784&amp;theme=dark"
-          width="280"
-          height="400"
-          allowtransparency="true"
-          frameborder="0"
-          sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-        ></iframe>
-      </div>
-
       <section
         id="vk-news"
         style={{
