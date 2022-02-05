484b053139341856ec36056dde4d542fa6fd6878
Yevhenii <koenigstag@gmail.com>
2022-02-05 12:51:12 +0200

    Fix node env not working

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,15 @@
-const {
+/* const {
   env: { NODE_ENV },
 } = process;
 
-const mode = NODE_ENV || 'development';
+const mode = NODE_ENV || 'development'; */
 
 const constants = {
   ACCESS_TOKEN: 'accessToken',
   REFRESH_TOKEN: 'refreshToken',
   ADMIN_TOKEN: 'ADMIN_TOKEN',
   SERVER_DOMAIN:
-    mode === 'development' ? 'localhost:5001' : 'tfc-survival.ru',
+    /* mode === 'development' ? 'localhost:5001' : */ 'tfc-survival.ru:5001',
   MAP_URL: 'http://tfc-survival.ru:8154/',
   SERVER_PING_URL: 'https://eu.mc-api.net/v3/server/ping/tfc-survival.ru',
 };
