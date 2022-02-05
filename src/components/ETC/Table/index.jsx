6f81b63a1a0096c10b05ff31e5fc3fe95674c7e1
Yevhenii <koenigstag@gmail.com>
2022-02-05 16:20:11 +0200

    Fix duplicate keys

--- src/components/ETC/Table/index.jsx
+++ src/components/ETC/Table/index.jsx
@@ -1,17 +1,33 @@
-import TableRow from './TableRow'
-import React from 'react'
+import TableRow from "./TableRow";
+import React from "react";
 
-export default function Table({ style, className, list, sortFunc, paths, headers, itemKey }) {
+export default function Table({
+  style,
+  className,
+  list,
+  sortFunc,
+  paths,
+  headers,
+  itemKey,
+}) {
   return (
-    <table className={className} style={style} >
+    <table className={className} style={style}>
       <thead>
         <tr>
-          {headers.map((header) => <td key={header}>{header}</td>)}
+          {headers.map((header) => (
+            <td key={header}>{header}</td>
+          ))}
         </tr>
       </thead>
       <tbody>
-        {list.sort(sortFunc).map(data => <TableRow data={data} paths={paths} key={data[itemKey]} />)}
+        {list.sort(sortFunc).map((data) => (
+          <TableRow
+            data={data}
+            paths={paths}
+            key={data[itemKey] + '_' + Math.random() * 100}
+          />
+        ))}
       </tbody>
     </table>
-  )
+  );
 }
