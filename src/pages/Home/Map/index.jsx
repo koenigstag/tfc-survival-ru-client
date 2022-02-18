1f4e837fd4d4077d8138a38147bced44dae52513
Yevhenii <koenigstag@gmail.com>
2022-02-18 21:06:56 +0200

    Add actual map link

--- src/pages/Home/Map/index.jsx
+++ src/pages/Home/Map/index.jsx
@@ -15,42 +15,49 @@ const MapPage = () => {
   }, []); */
 
   return (
-    <div
-      style={{
-        // background: `url(${blank_map}) no-repeat`,
-        backgroundSize: "100% 100%",
-        // backgroundPosition: 'center',
-        position: "absolute",
-        top: "1rem",
-        left: "1rem",
-        bottom: "1rem",
-        right: "1rem",
-        zIndex: 1,
-
-        margin: "auto",
-      }}
-    >
-      {/* <div style={{ width: '300px', height: '300px' }} dangerouslySetInnerHTML={{ __html: htmlMap }}></div> */}
-      <iframe
+    <>
+      <div
         style={{
+          // background: `url(${blank_map}) no-repeat`,
+          backgroundSize: "100% 100%",
+          // backgroundPosition: 'center',
           position: "absolute",
-          top: "0",
-          left: 0,
-          bottom: "0",
-          right: 0,
-          zIndex: 2,
+          top: "1rem",
+          left: "1rem",
+          bottom: "1rem",
+          right: "1rem",
+          zIndex: 1,
 
-          margin: "auto",
-          width: "95%",
-          height: "90%",
+          margin: "20px auto",
         }}
-        title="tfc-survival.ru dynmap"
-        // src={constants.MAP_URL}
-        src='/dynmap-web/index.html'
       >
-        Something went wrong...
-      </iframe>
-    </div>
+        {/* <div style={{ width: '300px', height: '300px' }} dangerouslySetInnerHTML={{ __html: htmlMap }}></div> */}
+        <iframe
+          style={{
+            position: "absolute",
+            top: "0",
+            left: 0,
+            bottom: "0",
+            right: 0,
+            zIndex: 2,
+
+            margin: "auto",
+            width: "95%",
+            height: "90%",
+          }}
+          title="tfc-survival.ru dynmap"
+          // src={constants.MAP_URL}
+          src="/dynmap-web/index.html"
+        >
+          Something went wrong...
+        </iframe>
+        
+      </div>
+      <h6>
+        Актуальная карта в реальном времени по{" "}
+        <a href="http://tfc-survival.ru:8154/">ссылке</a>
+      </h6>
+    </>
   );
 };
 
