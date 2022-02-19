b721844c7d73498be8e4ab611dc7dcd7bfce8808
Yevhenii <koenigstag@gmail.com>
2022-02-19 20:16:40 +0200

    Fix server domain

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ const constants = {
   ACCESS_TOKEN: 'accessToken',
   REFRESH_TOKEN: 'refreshToken',
   ADMIN_TOKEN: 'ADMIN_TOKEN',
-  SERVER_DOMAIN: 'tfc-survival.ru:5001',
+  SERVER_DOMAIN: 'https://tfc-survival.ru:5001',
   MAP_URL: 'http://tfc-survival.ru:8154/',
   SERVER_PING_URL: 'https://eu.mc-api.net/v3/server/ping/tfc-survival.ru',
 };
