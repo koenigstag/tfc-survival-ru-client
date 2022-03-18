1d33b0d219169b6dd0c74e9920f90689653d667f
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-03-18 19:21:52 +0200

    Add fix wip

--- public/news/assets/js/api/vk.mjs
+++ public/news/assets/js/api/vk.mjs
@@ -1,5 +1,9 @@
 import { client } from './index.mjs';
 
 export async function getVKNews() {
-  return await client.get('/vknews');
+  return await client.get('/vknews', {
+    headers: {
+      Origin: 'https://tfc-survival.ru',
+    },
+  });
 }
