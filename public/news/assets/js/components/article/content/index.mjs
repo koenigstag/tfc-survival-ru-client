16f0c1348abdf36a8aebf19fa93c5dea77ee7cc1
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-03-18 22:09:22 +0200

    Add styles

--- public/news/assets/js/components/article/content/index.mjs
+++ public/news/assets/js/components/article/content/index.mjs
@@ -2,6 +2,73 @@ export default function ArticleContent(props) {
   return `
     <section>
       ${props.text.replaceAll('\n', '<br/>')}
+      <div>
+        ${props.attachments?.map((att) => {
+          let tag;
+          if (att.type === 'photo') {
+            tag = `
+            <div style="text-align: center; margin-top: 20px;">
+              <img
+                style="
+                  display: inline-block;
+                  ${props.attachments.length === 1 ? 'margin: 0 auto;' : ''}
+                  max-width: 80%;
+                "
+                src="${att.photo.sizes[att.photo.sizes.length - 1].url}"
+                alt="${att.photo.text}"
+              />
+            </div>
+            `;
+          }
+          if (att.type === 'video') {
+            tag = `
+            <div style="display: flex; flex-direction: column; align-items: center;" >
+              <div style="display: flex; margin-bottom: 10px;" >
+                <a
+                  style="display: flex; position: relative;"
+                  target="_blank"
+                  rel="noreferrer"
+                  href="${`https://vk.com/tfcsurvivalru?z=video${props.attachments[0].video.owner_id}_${props.attachments[0].video.id}`}"
+                >
+                  <img
+                    style="
+                      display: block;
+                      width: 90%;
+                      margin: 0 auto;
+                    "
+                    src="${
+                      props.attachments[0].video.image[
+                        props.attachments[0].video.image.length - 1
+                      ].url
+                    }"
+                    alt="${props.attachments[0].video.title}"
+                  />
+                  <img
+                    style="
+                      position: absolute;
+                      align-self: center;
+                      left: calc(50% - 25px);
+                      width: 50px;
+                    "
+                    src="./assets/img/play-button.png"
+                    alt="play"
+                  />
+                </a>
+              </div>
+              <a
+                style="text-align: center;"
+                target="_blank"
+                rel="noreferrer"
+                href="${`https://vk.com/tfcsurvivalru?z=video-${props.attachments[0].video.owner_id}_${props.attachments[0].video.id}`}"
+              >
+                ${props.attachments[0].video.title}
+              </a>
+            </div>`;
+          }
+
+          return tag;
+        }).join('')}
+      </div>
     </section>
   `;
 }
--- public/news/assets/js/components/article/index.mjs
+++ public/news/assets/js/components/article/index.mjs
@@ -3,7 +3,12 @@ import ArticleHead from './header/index.mjs';
 
 export default function Article(props) {
   return `
-    <article style="margin-top: 10px; padding: 10px 20px 20px; border-bottom: 5px solid #f0f0f0; font-family: sans-serif;">
+    <article style="
+      margin-top: 10px;
+      padding: 10px 20px 20px;
+      border-bottom: 5px solid #f0f0f0;
+      font-family: sans-serif;
+    ">
       ${ArticleHead(props)}
       ${ArticleContent(props)}
     </article>
