71a242f1e0a9c9b9da5186f256eb9e7a3c9ea315
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-03-19 15:56:19 +0200

    Fix border

--- public/news/assets/js/components/article/index.mjs
+++ public/news/assets/js/components/article/index.mjs
@@ -8,7 +8,7 @@ export default function Article(props) {
       style="
         margin-top: 10px;
         padding: 10px 20px 20px;
-        border-bottom: 5px solid #a0a0a0;
+        border-bottom: 3px solid rgba(120, 120, 120, 0.3);
         font-family: sans-serif;
       "
     >
