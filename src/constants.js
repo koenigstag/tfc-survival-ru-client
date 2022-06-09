15660971d64a58ea08895221d512c4763895acec
Yevhenii Lapshynov <eugeniy.lapshynov@freshcodeit.com>
2022-06-09 18:38:57 +0300

    Change domain

--- src/constants.js
+++ src/constants.js
@@ -2,9 +2,11 @@ const constants = {
   ACCESS_TOKEN: 'accessToken',
   REFRESH_TOKEN: 'refreshToken',
   ADMIN_TOKEN: 'ADMIN_TOKEN',
-  SERVER_DOMAIN: 'https://tfc-survival.ru:3001',
+  // SERVER_DOMAIN: 'https://tfc-survival.ru:3001',
+  SERVER_DOMAIN: 'https://tfc.su:3001',
   MAP_URL: 'http://tfc-survival.ru:8154/',
-  SERVER_PING_URL: 'https://tfc-survival.ru:3001/api/ping',
+  // SERVER_PING_URL: 'https://tfc-survival.ru:3001/api/ping',
+  SERVER_PING_URL: 'https://tfc.su:3001/api/ping',
 };
 
 export default constants;
